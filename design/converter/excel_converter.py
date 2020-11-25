e3a20a3f152afd5a3b06e97b9e36a887083d5a9c
boyism80 <boyism80@gmail.com>
2020-11-25 22:44:49 +0900

    엑셀 컨버터에서 테이블 코드 제너레이트

--- design/converter/excel_converter.py
+++ design/converter/excel_converter.py
@@ -93,6 +93,27 @@ def sheet_class(cname, header):
             'client': template.render({'name': cname, 'property': properties['client']})}
 
 
+def sheet_table(table_name, class_name, sheet_name, header):
+    loader = jinja2.FileSystemLoader('templates')
+    env = jinja2.Environment(loader=loader)
+
+    result = {'server': None, 'client': None}
+    for dest in result:
+        key = [x['type']
+               for x in header if x['name'].startswith('*') and (x['dest'] == dest or x['dest'] == 'common')]
+        if len(key) == 0:
+            template = env.get_template('table_list.txt')
+            result[dest] = template.render(
+                {'sheetName': sheet_name, 'className': class_name, 'tableName': table_name})
+        else:
+            key = key[0]
+
+            template = env.get_template('table_dict.txt')
+            result[dest] = template.render(
+                {'sheetName': sheet_name, 'keyType': key, 'className': class_name, 'tableName': table_name})
+    return result
+
+
 if __name__ == '__main__':
     try:
         parser = argparse.ArgumentParser(description='Excel table converter')
@@ -106,6 +127,13 @@ if __name__ == '__main__':
         if os.path.isdir(args.out):
             shutil.rmtree(args.out)
 
+        os.makedirs(f'{args.out}/json/server', exist_ok=True)
+        os.makedirs(f'{args.out}/json/client', exist_ok=True)
+        os.makedirs(f'{args.out}/class/server', exist_ok=True)
+        os.makedirs(f'{args.out}/class/client', exist_ok=True)
+        os.makedirs(f'{args.out}/table/server', exist_ok=True)
+        os.makedirs(f'{args.out}/table/client', exist_ok=True)
+        table_codes = {'server': [], 'client': []}
         for excel in [os.path.join(args.dir, f) for f in os.listdir(args.dir) if os.path.isfile(os.path.join(args.dir, f)) and f.endswith('.xlsx') and not f.startswith('~$')]:
             workbook = openpyxl.load_workbook(excel, data_only=True)
             for sheet_name in workbook.sheetnames:
@@ -113,19 +141,32 @@ if __name__ == '__main__':
                 header = sheet_header(sheet)
                 body = sheet_body(sheet, header)
 
-                os.makedirs(f'{args.out}/json/server', exist_ok=True)
-                os.makedirs(f'{args.out}/json/client', exist_ok=True)
                 for dest in body:
                     with open(f'{args.out}/json/{dest}/{sheet_name}.json', 'w', encoding='utf8') as f:
                         f.write(json.dumps(
                             body[dest], indent=4, ensure_ascii=False))
 
-                os.makedirs(f'{args.out}/class/server', exist_ok=True)
-                os.makedirs(f'{args.out}/class/client', exist_ok=True)
                 class_name = sheet_name.capitalize()
                 classes = sheet_class(class_name, header)
                 for dest in classes:
                     with open(f'{args.out}/class/{dest}/{class_name}.cs', 'w', encoding='utf8') as f:
                         f.write(classes[dest])
+
+                table_name = f'Table{class_name}'
+                tcode_pair = sheet_table(
+                    table_name, class_name, sheet_name, header)
+                table_codes['server'].append(tcode_pair['server'])
+                table_codes['client'].append(tcode_pair['client'])
+
+        loader = jinja2.FileSystemLoader('templates')
+        env = jinja2.Environment(loader=loader)
+        template = env.get_template('table.txt')
+
+        for dest in table_codes:
+            table_codes[dest] = '\n'.join(
+                [f'    {x}' for x in '\n\n'.join(table_codes[dest]).split('\n')])
+            with open(f'{args.out}/table/{dest}/Table.cs', 'w', encoding='utf8') as f:
+                f.write(template.render({'contents': table_codes[dest]}))
+
     except Exception as e:
         logging.error(str(e))
--- design/update.bat
+++ design/update.bat
@@ -1,15 +1,21 @@
 PUSHD converter
 CALL excel_converter.exe --dir=.. --out=output
 
+RMDIR /s/q ..\..\client\ClientShared\MasterDataType
+ROBOCOPY output\class\client ..\..\client\ClientShared\MasterDataType
+RMDIR /s/q ..\..\server\ServerShared\MasterDataType
+ROBOCOPY output\class\server ..\..\server\ServerShared\MasterDataType
+
 RMDIR /s/q ..\..\client\ClientShared\Table
-ROBOCOPY output\class\client ..\..\client\ClientShared\Table
+ROBOCOPY output\table\client ..\..\client\ClientShared\Table
 RMDIR /s/q ..\..\server\ServerShared\Table
-ROBOCOPY output\class\server ..\..\server\ServerShared\Table
+ROBOCOPY output\table\server ..\..\server\ServerShared\Table
 
 RMDIR /s/q ..\..\client\ClientShared\Json
 ROBOCOPY output\json\client ..\..\client\ClientShared\Json
 RMDIR /s/q ..\..\server\ServerShared\Json
-ROBOCOPY output\json\server ..\..\server\ServerShared\Json
+RMDIR /s/q ..\..\server\TestServer\bin\Debug\net5.0\Json
+ROBOCOPY output\json\server ..\..\server\TestServer\bin\Debug\net5.0\Json
 POPD
 
 PAUSE
