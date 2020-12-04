fbb66f9582b82fc99dcc341d5f88ea38b01381aa
boyism80 <boyism80@gmail.com>
2020-12-05 00:44:21 +0900

    flatbuffer에 벡터타입 추가 지원

--- service/extension/flatb_extension.py
+++ service/extension/flatb_extension.py
@@ -4,38 +4,80 @@ import re
 import jinja2
 import argparse
 
+flat_type_dict = {'StringOffset': 'string'}
+
+def convert_type(type):
+    global flat_type_dict
+    for f, c in flat_type_dict.items():
+        type = type.replace(f, c)
+    return type
+    
+def extract(regex, code):
+    result = re.search(regex, code)
+    return {} if not result else result.groupdict()
+
+def extract_all(regex, code):
+    regex = re.compile(regex)
+    return [{} if not x else x.groupdict() for x in regex.finditer(code)]
+
+def allocate_code(type, this, param):
+    if type in offsetFuncDict:
+        return offsetFuncDict[type](param)
+
+    groups = extract(r'List<(?P<type>\b\w+)>', type)
+    if groups:
+        generic = groups['type']
+
+        middle = ''
+        if generic in offsetFuncDict:
+            middle = f".Select(x => {offsetFuncDict[generic]('x')})"
+        return f'Create{this.capitalize()}Vector(builder, {this}{middle}.ToArray())'
+
+def createStringCode(param):
+    return f"builder.CreateString({param})"
+
+offsetFuncDict = {'string': createStringCode}
+
 
 def method(code):
+    matchedTypeDict = {'String': 'string', 'Vector': 'List'}
+
     loader = jinja2.FileSystemLoader('templates')
     env = jinja2.Environment(loader=loader)
     method_template = env.get_template('method.txt')
 
-    result = re.search(
-        r'public static Offset<.+> Create(?P<name>.+)\(FlatBufferBuilder builder,\s*(?P<params>[\w\s=,().]*)\)', code)
-    group_dict = result.groupdict()
-    name = group_dict['name']
+    groups = extract_all(r'Create(?P<name>(\w(?!Block))+)\(FlatBufferBuilder builder, (?P<params>[a-zA-Z_\[\]]+).+\) {', code)
+    for x in groups:
+        params = [convert_type(x) for x in x['params'].split(', ')]
+        x['params'] = ', '.join(params)
 
-    parsed_params = [{'type': x['type'], 'name': x['name']} for x in re.finditer(
-        r'(?P<type>\b\w+) (?P<name>\b\w+) = .+', group_dict['params'])]
+    groups = extract(r'public static Offset<.+> Create(?P<name>.+)\(FlatBufferBuilder builder,\s*(?P<params>[\w\s=,().]*)\)', code)
+    name = groups['name']
 
-    parameters = ', '.join([
-        f"{x['type']} {x['name']}" for x in parsed_params])
-    parameters = parameters.replace('StringOffset', 'string')
-    parameters = parameters.replace('Offset', '')
+    parsed_params = [{'type': x['type'], 'name': x['name'], 'pure name': x['name'].replace('Offset', '')} for x in re.finditer(r'(?P<type>\b\w+) (?P<name>\b\w+) = .+', groups['params'])]
+    for parsed in parsed_params:
+        parsed['type'] = parsed['type'].replace('Offset', '')
+        if parsed['type'] in matchedTypeDict:
+            parsed['type'] = matchedTypeDict[parsed['type']]
+
+        if parsed['type'] == 'List':
+            groups = extract(rf"\spublic (?P<type>\b\w+) {parsed['pure name'].capitalize()}\(int j\)", code)
+            parsed['type'] = f"List<{groups['type']}>"
+
+
+    parameters = ', '.join([f"{x['type']} {x['pure name']}" for x in parsed_params])
 
     arguments = ', '.join([f"{x['name']}" for x in parsed_params])
 
-    params_str = [x['name']
-                  for x in parsed_params if x['type'] == 'StringOffset']
-    stringCodes = [
-        f"  var {x} = builder.CreateString({x.replace('Offset', '')});" for x in params_str]
-    stringCodes = '\n'.join(stringCodes)
+    offsets = [(x, allocate_code(x['type'], x['pure name'], x['pure name'])) for x in parsed_params]
+    offsets = [f"  var {x['name']} = {code};" for (x, code) in offsets if code]
+    offsets = '\n'.join(offsets)
 
     code = method_template.render({'flatbName': name,
                                    'flatbLowerName': name.lower(),
                                    'parameters': parameters,
                                    'arguments': arguments,
-                                   'stringCodes': stringCodes})
+                                   'offsets': offsets})
     code = code.split('\n')
     code = [f'  {x}' for x in code]
     code = '\n'.join(code)
@@ -56,7 +98,7 @@ if __name__ == '__main__':
             with open(file, 'r', encoding='utf8') as f:
                 data = f.read()
                 code = method(data)
-                data = f"{data[:-4]}\n{code}\n{data[-4:]}"
+                data = f"using System.Linq;\n{data[:-4]}\n{code}\n{data[-4:]}"
 
             with open(file, 'w', encoding='utf8') as f:
                 f.write(data)
